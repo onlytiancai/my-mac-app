99ce417520cdfc883ce501a0fe5e3d2d59925bd1
onlytiancai <onlytiancai@gmail.com>
2025-07-26 07:35:00 +0800

    Initial Commit

---
+++ my-mac-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  my-mac-app
+//
+//  Created by 胡浩 on 2025/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ my-mac-app/my_mac_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_mac_appApp.swift
+//  my-mac-app
+//
+//  Created by 胡浩 on 2025/7/26.
+//
+
+import SwiftUI
+
+@main
+struct my_mac_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
